a1f02911fff5b3c0968269b313fa5d30d279827c
Luke Simpson <luke.simpson@ruralsourcing.com>
2023-03-14 14:42:15 -0500

    remove slowwly

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use futures::Future;
 use reqwest;
-use reqwest::Client as BlockingClient;
+use reqwest::Client;
 
 static REDDIT: &str = "http://www.reddit.com/r/rust.json";
-static SLOWWLY: &str =
-    "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.reddit.com/r/rust.json";
 
-fn get_request(builder: reqwest::RequestBuilder) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
+fn get_rust_posts(
+    _req: actix_web::HttpRequest,
+    client: web::Data<Client>,
+) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
+    let builder = client.get(REDDIT);
     actix_web::web::block(move || builder.send())
         .from_err()
         .and_then(|mut res| match res.text() {
@@ -23,34 +25,14 @@ fn get_request(builder: reqwest::RequestBuilder) -> impl Future<Item = HttpRespo
         })
 }
 
-fn get_rust_posts(
-    _req: actix_web::HttpRequest,
-    client: web::Data<BlockingClient>,
-) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
-    let builder = client.get(REDDIT);
-    get_request(builder)
-}
-
-fn get_rust_posts_slowwly(
-    _req: actix_web::HttpRequest,
-    client: web::Data<BlockingClient>,
-) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
-    let builder = client.get(SLOWWLY);
-    get_request(builder)
-}
-
 fn main() {
     HttpServer::new(|| {
         App::new()
-            .data(BlockingClient::new())
+            .data(Client::new())
             .wrap(middleware::Logger::default())
             .service(web::resource("/get/rust/posts").route(web::get().to_async(get_rust_posts)))
-            .service(
-                web::resource("/get/rust/posts/slowwly")
-                    .route(web::get().to_async(get_rust_posts_slowwly)),
-            )
     })
-    .bind("127.0.0.1:8000")
+    .bind("127.0.0.1:5000")
     .unwrap()
     .run()
     .unwrap();
